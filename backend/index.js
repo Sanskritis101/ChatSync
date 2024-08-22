e7ded7b332f4a319bff5c8a87f08cee3bc2d278f
Sanskriti <104347305+Sanskritis101@users.noreply.github.com>
2024-08-22 16:07:19 +0530

    Final

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const axios = require("axios");
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username},
+        { headers: { "Private-Key": "5edc888d-5fd1-4858-8e32-f4e8b4032ac6" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  } 
+});
+
+app.listen(3001);
